a835a39a3a25716a4a85fd3bf3be53a02b9a8a23
amrkh97 <amrkh97@gmail.com>
2020-03-31 16:31:32 +0200

    Extracted top 5~6 teams from major 5 leagues.

--- main.R
+++ main.R
@@ -12,7 +12,7 @@ f19 <- read.csv("Dataset/players_19.csv")
 f20 <- read.csv("Dataset/players_20.csv")
 
 
-unnecessaryColumns <- c("sofifa_id","player_url","dob","long_name",
+unnecessaryColumns <- c("sofifa_id","player_url","dob","short_name",
                         "relaease_clause_eur","real_face","nation_position",
                         "nation_jersey_number","mentality_composure")
 
@@ -24,3 +24,16 @@ f18 <- f18[,!(names(f18) %in% unnecessaryColumns)]
 f19 <- f19[,!(names(f19) %in% unnecessaryColumns)]
 f20 <- f20[,!(names(f20) %in% unnecessaryColumns)]
 
+top5leagues <- c("Arsenal","Manchester United","Manchester City","Liverpool","Tottenham Hotspur","Chelsea",
+                 "FC Barcelona","AtlÃ©tico Madrid","Real Madrid","AtlÃ©tico Madrid","Sevilla","Valencia CF",
+                 "Napoli","Juventus","Inter","Lazio","Milan","Atalanta","Roma",
+                 "Borussia Dortmund","FC Bayern MÃ¼nchen","RB Leipzig","Bayer 04 Leverkusen","Borussia MÃ¶nchengladbach",
+                 "Paris Saint-Germain","Olympique Lyonnais","LOSC Lille","Stade Rennais FC","AS Monaco")
+
+
+top2015 <- f15[f15$club %in% top5leagues,]
+top2016 <- f16[f16$club %in% top5leagues,]
+top2017 <- f17[f17$club %in% top5leagues,]
+top2018 <- f18[f18$club %in% top5leagues,]
+top2019 <- f19[f19$club %in% top5leagues,]
+top2020 <- f20[f20$club %in% top5leagues,]
