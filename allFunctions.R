c96f8046f4b2178dad21afa2b77494fbe31f7804
Abdelgawad <elgawad.mostafa@gmail.com>
2020-05-01 16:59:16 +0200

    moved all KNN mode functions to function.r

--- allFunctions.R
+++ allFunctions.R
@@ -1047,3 +1047,50 @@ LinearRegressionPreProcessing <- function (df)
                         defending_sliding_tackle) 
     return(temp)
   }
+
+###########################################################################
+###########################################################################
+####################### KNN wage prediction ###############################
+###########################################################################
+knnNormalize <- function(x){
+  return ((x - min(x)) / (max(x) - min(x)))
+}
+knnPositionPrediction <- function(df, df.n){
+  dat.d <- sample(1:nrow(df.n),size=nrow(df.n)*0.7,replace = FALSE)
+  train.fifa <- df.n[dat.d,]
+  test.fifa <- df.n[-dat.d,]
+  train.fifa_labels <- df[dat.d,ncol(df)]
+  test.fifa_labels <-df[-dat.d,ncol(df)]
+  #print(NROW(train.fifa_labels))
+  knn.73 <- knn(train=train.fifa, test=test.fifa, cl=train.fifa_labels, k=107)
+  ACC.73 <- 100 * sum(test.fifa_labels == knn.73)/NROW(test.fifa_labels)
+  ACC.73
+  #table(knn.73 ,test.fifa_labels)
+  #knn.73
+  #confusionMatrix(table(knn.73 ,test.fifa_labels))
+}
+prepareKNNPredictionDataWages <- function(df){
+  df.subset <- df[,c("pace","shooting","passing","dribbling","defending","physic","attacking_crossing","attacking_finishing",
+                     "attacking_heading_accuracy","attacking_short_passing","attacking_volleys","skill_dribbling",
+                     "skill_curve","skill_fk_accuracy","skill_long_passing","skill_ball_control","movement_acceleration",
+                     "movement_sprint_speed","movement_agility","movement_reactions","movement_balance","power_shot_power",
+                     "power_jumping","power_stamina","power_strength","power_long_shots","mentality_aggression",
+                     "defending_marking","defending_standing_tackle","defending_sliding_tackle", "wage_brackets")]
+                     "mentality_interceptions","mentality_positioning","mentality_vision","mentality_penalties",
+  df.subset <- df.subset[complete.cases(df.subset), ]
+  return(df.subset)
+}
+knnWagesPrediction <- function(df, df.n){
+  dat.d <- sample(1:nrow(df.n),size=nrow(df.n)*0.7,replace = FALSE)
+  train.fifa <- df.n[dat.d,]
+  test.fifa <- df.n[-dat.d,]
+  train.fifa_labels <- df[dat.d,ncol(df)]
+  #NROW(train.fifa_labels)
+  test.fifa_labels <-df[-dat.d,ncol(df)]
+  knn.73 <- knn(train=train.fifa, test=test.fifa, cl=train.fifa_labels, k=107)
+  ACC.73 <- 100 * sum(test.fifa_labels == knn.73)/NROW(test.fifa_labels)
+  #table(knn.73 ,test.fifa_labels)
+  ACC.73
+  #confusionMatrix(table(knn.73 ,test.fifa_labels))
+  #knn.73
+}
