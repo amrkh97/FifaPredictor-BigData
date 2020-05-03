396f1302da546c5b53a1a61d9861292264385e0a
amrkh97 <amrkh97@gmail.com>
2020-05-03 12:54:38 +0200

    Re-organized the code
    - Separated FIFA 20 visualizations for a better understanding of the graphs steps.

--- main.R
+++ main.R
@@ -8,68 +8,146 @@ getwd()
 source("allFunctions.R")
 
 
+
+# Get unnecessary columns to be removed:
+unnecessaryColumns <- c("sofifa_id","player_url","dob","short_name",
+                        "real_face","nation_position",
+                        "nation_jersey_number","mentality_composure","loaned_from",
+                        "team_position","team_jersey_number","joined","contact_valid_until")
+
+
+##################################### FIFA 20 #####################################
+f20_complete <- read.csv("Dataset/players_20.csv")
+f20 <- f20_complete[,!(names(f20_complete) %in% unnecessaryColumns)]
+f20 <- handleNonNumericAttributes(f20)
+f20 <- addPositionColumn(f20)
+
+
+logisticRegression_Position(f20)
+
+
+# Age Graphs:
+
+g_age <- ggplot(data = f20, aes(age))
+
+g_age + geom_histogram(bins = 25,col="orange", aes(fill = ..count..)) + ggtitle("Fifa 20:Distribution based on Age")
+
+g_age +
+  geom_density(col="orange", aes(fill = Position), alpha=0.5) + facet_grid(.~Position) + 
+  ggtitle("Fifa 20:Distribution based on Age and Position")
+
+ggplot(f20, aes(Position)) + 
+  geom_bar(aes(fill = ..count..)) + 
+  ggtitle("Fifa 20:Distribution based on General Playing Position")
+
+
+# Top 10 countries info:
+
+graphTopCountries(f20)
+
+
+# Draw Top 10 Countries on worldmap:
+
+countries_count <- count(f20, nationality)
+top10 <- top_n(countries_count, 10, n)
+top10 <- as.data.frame(top10)
+# Work-Around to graph UK
+levels(top10$nationality) <- c(levels(top10$nationality), "UK") 
+top10$nationality[top10$nationality == "England"]  <- "UK"
+
+fr <- joinCountryData2Map(dF = top10,joinCode = "NAME",nameJoinColumn = "nationality",verbose=F) # Prepare data to plot
+
+mapCountryData(mapToPlot = fr,nameColumnToPlot = "nationality",
+               catMethod = "fixedWidth",
+               oceanCol = "steelblue1",
+               missingCountryCol = "white",
+               mapTitle = "Number of players per Country",
+               colourPalette = "heat",
+               aspect = "variable",
+               addLegend = F)
+
+
+# Wage & Value Statistics:
+
+f20 <- addWageandValueLevels(f20)
+plotWagesMoreThan100k(f20)
+plotValueAbove30M(f20)
+
+
+# Overall & Age relation:
+
+plotAgevsOverall(f20)
+
+
+# Club Value:
+
+plotTopClubValue(f20)
+
+
+# SVM Position Prediction:
+
+CMSVM_F20 <- predictSVM(f20)
+CMSVM_F20
+
+
+# Random Forest Position Prediction:
+
+CMRF_F20 <- predictRandomForrest(f20)
+CMRF_F20
+
+
+# Attribtes Correlation HeatMap:
+
+plotCorrelationHeatMap(f20)
+
+
+# Best team in FIFA based on overall:
+
+plotBest442("20")
+plotBest352("20")
+plotBest433("20")
+
+F20_442 <- getBestTeamForFormation("20", "4-4-2")
+F20_352 <- getBestTeamForFormation("20", "3-5-2")
+F20_433 <- getBestTeamForFormation("20", "4-3-3")
+
+bestTeams20 <- list(F20_442, F20_352, F20_433)
+bestTeams20[getBestTeamByOverall(F20_442, F20_352, F20_433)]
+
+
+###################################### FIFA 16 --> FIFA 19 ######################################
 # Read datasets
 f16_complete <- read.csv("Dataset/players_16.csv")
 f17_complete <- read.csv("Dataset/players_17.csv")
 f18_complete <- read.csv("Dataset/players_18.csv")
 f19_complete <- read.csv("Dataset/players_19.csv")
-f20_complete <- read.csv("Dataset/players_20.csv")
 
-
-
-# Get unnecessary columns
-unnecessaryColumns <- c("sofifa_id","player_url","dob","short_name",
-                        "real_face","nation_position",
-                        "nation_jersey_number","mentality_composure","loaned_from",
-                        "team_position","team_jersey_number","joined","contact_valid_until")
 
 # Remove unnecessary columns
 f16 <- f16_complete[,!(names(f16_complete) %in% unnecessaryColumns)]
 f17 <- f17_complete[,!(names(f17_complete) %in% unnecessaryColumns)]
 f18 <- f18_complete[,!(names(f18_complete) %in% unnecessaryColumns)]
 f19 <- f19_complete[,!(names(f19_complete) %in% unnecessaryColumns)]
-f20 <- f20_complete[,!(names(f20_complete) %in% unnecessaryColumns)]
-# Get top 5 leagues
-top5leagues <- c("Arsenal","Manchester United","Manchester City","Liverpool","Tottenham Hotspur","Chelsea",
-                 "FC Barcelona","AtlÃ©tico Madrid","Real Madrid","AtlÃ©tico Madrid","Sevilla","Valencia CF",
-                 "Napoli","Juventus","Inter","Lazio","Milan","Atalanta","Roma",
-                 "Borussia Dortmund","FC Bayern MÃ¼nchen","RB Leipzig","Bayer 04 Leverkusen","Borussia MÃ¶nchengladbach","FC Schalke 04",
-                 "Paris Saint-Germain","Olympique Lyonnais","LOSC Lille","Stade Rennais FC","AS Monaco")
 
-# WILL BE USED LATER
-# Keep top 5 leagues only
-#top2016 <- f16[f16$club %in% top5leagues,]
-#top2017 <- f17[f17$club %in% top5leagues,]
-#top2018 <- f18[f18$club %in% top5leagues,]
-#top2019 <- f19[f19$club %in% top5leagues,]
-#top2020 <- f20[f20$club %in% top5leagues,]
-
-####################################################################
 # Handle String attributes that caused errors:
 # For Example: attacking_crossing, ls and similar attributes.
 f16 <- handleNonNumericAttributes(f16)
 f17 <- handleNonNumericAttributes(f17)
 f18 <- handleNonNumericAttributes(f18)
 f19 <- handleNonNumericAttributes(f19)
-f20 <- handleNonNumericAttributes(f20)
 
-####################################################################
 # Factorise player positions:
 f16 <- addPositionColumn(f16)
 f17 <- addPositionColumn(f17)
 f18 <- addPositionColumn(f18)
 f19 <- addPositionColumn(f19)
-f20 <- addPositionColumn(f20)
 
-####################################################################
 # Logistic Regression For Defense:
 logisticRegression_Position(f16)
 logisticRegression_Position(f17)
 logisticRegression_Position(f18)
 logisticRegression_Position(f19)
-logisticRegression_Position(f20)
 
-####################################################################
 #Age Statistics:
 
 # Fifa 16:
@@ -125,150 +203,84 @@ ggplot(f19, aes(Position)) +
   geom_bar(aes(fill = ..count..)) + 
   ggtitle("Fifa 19:Distribution based on General Playing Position")
 
-# Fifa 20:
-g_age <- ggplot(data = f20, aes(age))
-
-g_age + geom_histogram(bins = 25,col="orange", aes(fill = ..count..)) + ggtitle("Fifa 20:Distribution based on Age")
-
-g_age +
-  geom_density(col="orange", aes(fill = Position), alpha=0.5) + facet_grid(.~Position) + 
-  ggtitle("Fifa 20:Distribution based on Age and Position")
-
-ggplot(f20, aes(Position)) + 
-  geom_bar(aes(fill = ..count..)) + 
-  ggtitle("Fifa 20:Distribution based on General Playing Position")
-
-
-########################################################################
 # Top 10 Countries:
 
 graphTopCountries(f16)
 graphTopCountries(f17)
 graphTopCountries(f18)
 graphTopCountries(f19)
-graphTopCountries(f20)
 
-# Draw Top 10 Countries In F20:
-
-countries_count <- count(f20, nationality)
-top10 <- top_n(countries_count, 10, n)
-top10 <- as.data.frame(top10)
-# Work-Around to graph UK
-levels(top10$nationality) <- c(levels(top10$nationality), "UK") 
-top10$nationality[top10$nationality == "England"]  <- "UK"
-
-fr <- joinCountryData2Map(dF = top10,joinCode = "NAME",nameJoinColumn = "nationality",verbose=F) # Prepare data to plot
-
-mapCountryData(mapToPlot = fr,nameColumnToPlot = "nationality",
-               catMethod = "fixedWidth",
-               oceanCol = "steelblue1",
-               missingCountryCol = "white",
-               mapTitle = "Number of players per Country",
-               colourPalette = "heat",
-               aspect = "variable",
-               addLegend = F)
-
-########################################################################
 # Add Wage and value Labels:
 
 f16 <- addWageandValueLevels(f16)
 f17 <- addWageandValueLevels(f17)
 f18 <- addWageandValueLevels(f18)
 f19 <- addWageandValueLevels(f19)
-f20 <- addWageandValueLevels(f20)
 
-#######################################################################
 # Wage Above 100k:
 
 plotWagesMoreThan100k(f16)
 plotWagesMoreThan100k(f17)
 plotWagesMoreThan100k(f18)
 plotWagesMoreThan100k(f19)
-plotWagesMoreThan100k(f20)
 
-######################################################################
+
 # Age vs Overall:
 
 plotAgevsOverall(f16)
 plotAgevsOverall(f17)
 plotAgevsOverall(f18)
 plotAgevsOverall(f19)
-plotAgevsOverall(f20)
 
-######################################################################
 # Market Value:
 
 plotValueAbove30M(f16)
 plotValueAbove30M(f17)
 plotValueAbove30M(f18)
 plotValueAbove30M(f19)
-plotValueAbove30M(f20)
 
-#####################################################################
 # Club Values:
 
 plotTopClubValue(f16)
 plotTopClubValue(f17)
 plotTopClubValue(f18)
 plotTopClubValue(f19)
-plotTopClubValue(f20)
 
-############################################################################
-# Predict using SVM:
+# Predict Position using SVM:
 
 CMSVM_F16 <- predictSVM(f16)
 CMSVM_F17 <- predictSVM(f17)
 CMSVM_F18 <- predictSVM(f18)
 CMSVM_F19 <- predictSVM(f19)
-CMSVM_F20 <- predictSVM(f20)
+
 
 CMSVM_F16
 CMSVM_F17
 CMSVM_F18
 CMSVM_F19
-CMSVM_F20
 
-############################################################################
-# Predict using Random Forrest:
+# Predict Position using Random Forrest:
 
 CMRF_F16 <- predictRandomForrest(f16)
 CMRF_F17 <- predictRandomForrest(f17)
 CMRF_F18 <- predictRandomForrest(f18)
 CMRF_F19 <- predictRandomForrest(f19)
-CMRF_F20 <- predictRandomForrest(f20)
+
 
 CMRF_F16
 CMRF_F17
 CMRF_F18
 CMRF_F19
-CMRF_F20
 
-
-############################################################################
 # Correlation:
 
 plotCorrelationHeatMap(f16)
 plotCorrelationHeatMap(f17)
 plotCorrelationHeatMap(f18)
 plotCorrelationHeatMap(f19)
-plotCorrelationHeatMap(f20)
 
 
-
-############################################################################
 # Best Team In Each Year:
-
-# FIFA 20:
-plotBest442("20")
-plotBest352("20")
-plotBest433("20")
-
-F20_442 <- getBestTeamForFormation("20", "4-4-2")
-F20_352 <- getBestTeamForFormation("20", "3-5-2")
-F20_433 <- getBestTeamForFormation("20", "4-3-3")
-
-bestTeams20 <- list(F20_442, F20_352, F20_433)
-bestTeams20[getBestTeamByOverall(F20_442, F20_352, F20_433)]
 
 # FIFA 19:
 plotBest442("19")
@@ -298,7 +310,8 @@ bestTeams18[getBestTeamByOverall(F18_442, F18_352, F18_433)]
 # F20 Team is better than that of other years:
 getBestTeamByOverall(F20_433, F19_433, F18_433)
 
-########################################################
+
+######################### Lionel Messi VS Cristiano Ronaldo ###############################
 # Lionel Messi VS Cristiano Ronaldo:
 
 plotMessiVsCristiano()
